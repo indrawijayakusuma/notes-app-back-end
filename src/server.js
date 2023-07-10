db00d89047fe285e5c202bed9eb79fd9a23c10a2
indrawijayakusuma <indrawk56@gmail.com>
2023-07-10 16:16:26 +0700

    database

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./service/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
-  const server = Hapi.Server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
       },
     },
   });
-  server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
